9af921db9ba6bae9e3abc1f48c9b31796ad3aca2
pray-lee <praycis@gmail.com>
2020-12-10 19:13:34 +0800

    update

--- proxy.js
+++ proxy.js
@@ -1,4 +1,5 @@
 // proxy 代理
+// proxy代理不会影响目标对象原有的属性和方法
 // 用于修改某些操作的默认行为, 相当于在语言层面做出修改，即对编程语言进行编程
 // proxy可以理解为在目标对象之前架设一层"拦截",外界对该对象的访问都必须先通过这层拦截
 // 基本结构
@@ -22,3 +23,40 @@ let handler = {}
 let proxy1 = new Proxy(target, handler)
 proxy1.a = 1
 console.log(target.a) // 1
+
+// 使用场景 get
+// 翻译字典
+let dict = {
+    hello: '你好',
+    fuckYou: '草泥马',
+    nice: '美丽'
+}
+dict = new Proxy(dict, {
+    get(target, key) {
+        if (key in target)
+            return target[key]
+        return key
+    }
+})
+console.log(dict.hello)
+console.log(dict.fuckYou)
+console.log(dict.nice)
+
+// ownKeys
+class A {
+    constructor() {
+
+    }
+    a() {}
+    b() {}
+    c() {}
+}
+let a = new Proxy(A.prototype, {
+    ownKeys(target) {
+        // 在这里拦截用户对目标对象的遍历: Object.getOwnPropertyNames, Object.getOwnPropertySymbols, Object.keys
+        // 要返回一个数组，对象的key, 在这里可以过滤不想让用户看到的属性
+        return Object.getOwnPropertyNames(target).filter(key => key !== 'a')
+    }
+})
+console.log(Object.keys(a)) // ['constructor', 'b' , 'c']
+
